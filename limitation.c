ccb98d83dfca89ffc2603ad8ff8fec2cd96e70e9
Abidur Niloy <niloyabidur@gmail.com>
2023-04-02 10:02:01 +0600

    dome my work today

---
+++ limitation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a =10000000;
+    long long b =123456789032;
+    printf("%d\n", a);
+    printf("%lld\n", b);
+
+    return 0;
+}
---
+++ variable.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    float karim = 10.3;
+    printf("%0.6f", karim);
+    return 0;
+}
