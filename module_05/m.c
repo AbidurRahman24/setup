d2d24a9ea8e62b0e9f7130b6db4ab20d605233c6
Abidur Niloy <niloyabidur@gmail.com>
2023-04-09 09:14:31 +0600

    module 5 done

---
+++ module_05/m.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    char x;
+    scanf("%c", &x);
+    if(x >='0' && x <= '9')
+    {
+        printf("IS DIGIT\n");
+    }
+    else{
+        printf("ALPHA\n");
+        if( x >= 'a' && x <= 'z'){
+            printf("IS SMALL");
+        }
+        else{
+            printf("IS CAPITAL\n");
+        }
+    }
+    return 0;
+}
