1fb9837274e849e4ed507370c9727aed469822da
Abidur Niloy <niloyabidur@gmail.com>
2023-04-19 17:26:25 +0600

    string operation and frequantly array

--- module/module 11/stremp.c
+++ module/module 11/stremp.c
@@ -6,17 +6,17 @@ int main(){
     char a[100], b[100];
     scanf("%s %s", a,b);
     int v = strcmp(a,b);
-    if(v < 0)
-    {
-        printf("A is smaller");
-    }
-    else if(v > 0)
-    {
-        printf("B is smaller");
-    }
-    else{
-        printf("same");
-    }
-    // printf("%d", v);
+    // if(v < 0)
+    // {
+    //     printf("A is smaller");
+    // }
+    // else if(v > 0)
+    // {
+    //     printf("B is smaller");
+    // }
+    // else{
+    //     printf("same");
+    // }
+    printf("%d", v);
     return 0;
 }
---
+++ module/module 11/tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+ // if(v < 0)
+    // {
+    //     printf("A is smaller");
+    // }
+    // else if(v > 0)
+    // {
+    //     printf("B is smaller");
+    // }
+    // else{
+    //     printf("same");
+    // }
